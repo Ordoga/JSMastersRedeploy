f475716c37874ad04432d009dbb0a12cde00c5ab
Ordoga <ordogga@gmail.com>
2024-11-26 20:46:57 +0200

    Broadcasting and updating code in real time

--- services/socket.service.js
+++ services/socket.service.js
@@ -8,8 +8,17 @@ export function setupSocketAPI(server) {
             origin: '*',
         },
     })
+
     gIo.on('connection', socket => {
         gIo.emit('user-connected', 'A user Has Connected')
+
+        // socket.on('entered-codeblock-page', data => {
+        //     socket.join(data.codeblockId)
+        // })
+
+        socket.on('changed-code', newCode => {
+            socket.broadcast.emit('update-code', newCode)
+        })
 
         socket.on('disconnect', socket => {
             console.log(`socket with fullname ${socket.name} disconnected`)
