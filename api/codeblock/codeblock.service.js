864988684536505eaf6632de90fe27afcb72d60d
Ordoga <ordogga@gmail.com>
2024-11-26 14:12:02 +0200

    Basic get request working

--- api/codeblock/codeblock.service.js
+++ api/codeblock/codeblock.service.js
@@ -6,6 +6,7 @@ async function query() {
     const codeBlocks = [
         { name: 'code1', id: '1' },
         { name: 'code2', id: '2' },
+        { name: 'code3', id: '3' },
     ]
     return codeBlocks
 }
--- server.js
+++ server.js
@@ -27,14 +27,22 @@ app.use(express.static(path.resolve('public')))
 //     res.sendFile(path.resolve('public/index.html'))
 // })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://localhost:5173'],
+        credentials: true,
+    }
+    app.use(cors(corsOptions))
+}
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/codeblock', codeblockRoutes)
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public/index.html'))
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
 })
 
 server.listen(port, () => console.log(`Server is running on port ${port}`))
